03485ec0606b0a8a8d05100402ca49f42621ecc6
jakegerb <72156977+jakegerb@users.noreply.github.com>
2020-10-01 13:06:17 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,5 @@
 def main():
+def primary():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +9,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  primary()
